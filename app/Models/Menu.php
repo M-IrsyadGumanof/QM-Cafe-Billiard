6350ee50ac6e7a53bfa37efc51da395b22ad257f
Muhammad Irsyad Gumanof <mirsadgumanof@gmail.com>
2026-06-16 23:56:09 +0700

    feat: implement menu item CRUD with file upload

---
+++ app/Models/Menu.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Menu extends Model
+{
+    protected $fillable = [
+        'menu_category_id',
+        'name',
+        'slug',
+        'description',
+        'price',
+        'image',
+        'stock',
+        'status'
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(MenuCategory::class, 'menu_category_id');
+    }
+
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
