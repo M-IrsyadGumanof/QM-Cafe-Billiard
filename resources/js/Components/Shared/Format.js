917fd37014c6831f5a98ec7e092f7af72cadd3d5
Suhafdal Zikri <suhafdalzikri@gmail.com>
2026-06-17 04:36:22 +0700

    style(customer-menu): refine menu display and shared formatting

--- resources/js/Components/Shared/Format.js
+++ resources/js/Components/Shared/Format.js
@@ -1,14 +1,14 @@
-export const money = (value = 0) => 
-  new Intl.NumberFormat('id-ID', { 
-    style: 'currency', 
-    currency: 'IDR', 
-    maximumFractionDigits: 0 
-  }).format(Number(value || 0));
+export const money = (value = 0) =>
+    new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        maximumFractionDigits: 0,
+    }).format(Number(value || 0));
 
-export const date = (value) => 
-  value ? new Intl.DateTimeFormat('id-ID', { 
-    dateStyle: 'medium' 
-  }).format(new Date(value)) : '-';
+export const date = (value) =>
+    value
+        ? new Intl.DateTimeFormat('id-ID', { dateStyle: 'medium' }).format(new Date(value))
+        : '-';
 
-export const textStatus = (value) => 
-  String(value || '-').replaceAll('_', ' ');
+export const textStatus = (value) =>
+    String(value || '-').replaceAll('_', ' ');
